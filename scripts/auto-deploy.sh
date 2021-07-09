5a020b3ac56e0aab8b2b3206038ddf30ffad684c
Kevin Gibbons <bakkot@gmail.com>
2021-07-09 00:04:32 -0700

    fix deploy script

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+
+declare -r GH_USER_EMAIL="bot@tc39.es"
+declare -r GH_USER_NAME="Self-deploy bot"
+declare -r COMMIT_MESSAGE="Update gh-pages"
+
+
 declare -r PRIVATE_KEY_FILE_NAME='github_deploy_key'
 
-if [ -z "${encrypted_1e2182e20f4c_iv-}" ] || [ -z "${encrypted_1e2182e20f4c_key}" ]; then
-  echo >&2 'secure keys do not exist'
-  exit 0
-fi
+cd "$(dirname "$BASH_SOURCE")"/..
+mkdir -p ~/.ssh
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo "${GH_DEPLOY_KEY}" > ~/.ssh/${PRIVATE_KEY_FILE_NAME} # GH_DEPLOY_KEY is defined in deploy.yml
+chmod 600 ~/.ssh/${PRIVATE_KEY_FILE_NAME}
 
-# Decrypt the file containing the private key
-
-openssl aes-256-cbc \
-    -K  $encrypted_1e2182e20f4c_key \
-    -iv $encrypted_1e2182e20f4c_iv \
-    -in "$(dirname "$BASH_SOURCE")/${PRIVATE_KEY_FILE_NAME}.enc" \
-    -out ~/.ssh/$PRIVATE_KEY_FILE_NAME -d
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Enable SSH authentication
-
-chmod 600 ~/.ssh/$PRIVATE_KEY_FILE_NAME
 echo "Host github.com" >> ~/.ssh/config
-echo "  IdentityFile ~/.ssh/$PRIVATE_KEY_FILE_NAME" >> ~/.ssh/config
+echo "  IdentityFile ~/.ssh/${PRIVATE_KEY_FILE_NAME}" >> ~/.ssh/config
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Update the content from the `gh-pages` branch
-
-$(npm bin)/update-branch --commands "npm run build-travis" \
-                         --commit-message "Update gh-pages [skip ci]" \
-                         --directory "out" \
-                         --distribution-branch "gh-pages" \
-                         --source-branch "master"
+cd out
+git config --global user.email "${GH_USER_EMAIL}"
+git config --global user.name "${GH_USER_NAME}"
+git config --global init.defaultBranch gh-pages
+git init
+git add -A
+git commit --message "${COMMIT_MESSAGE}"
+git push --force "git@github.com:${GITHUB_REPOSITORY}.git" gh-pages
